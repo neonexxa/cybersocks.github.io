c0ad82849c70cc28217584c6518213e0d421a578
Shahril Amri <github@moefi.xyz>
2016-07-25 09:29:26 +0800

    new quals date
    minor fixes
    - removed hover effect on navbar
    - removed &bef content on nav (finally fix that broken dropdown)
    - removed all comment from js to avoid error from html compression

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,25 @@
-// Greetings
 $(document).ready(function() {
 	console.log( "Welcome to the Cybersocks Regional Competition 2016!" );
 });
 
-// Initiates typeit
 $('#typing').typeIt({
-	cursor: false // Say no to blinking cursor
+	cursor: false
 });
 
-// mobile-friendly side-navigation
 $('.button-collapse').sideNav({
-	menuWidth: 300, // Default is 240
-	edge: 'right', // Choose the horizontal origin
-	closeOnClick: true // Closes side-nav on clicks
+	menuWidth: 300,
+	edge: 'right',
+	closeOnClick: true
 });
 
-// dropdown config
 $('.dropdown-button').dropdown({
 	inDuration: 300,
 	outDuration: 325,
-	constrain_width: false, // Does not change width of dropdown to that of the activator
-	hover: true, // Activate on hover
-	gutter: 0, // Spacing from edge
-	belowOrigin: true, // Displays dropdown below the button
-	alignment: 'left' // Displays dropdown with edge aligned to the left of button
+	constrain_width: false,
+	hover: true,
+	gutter: 0,
+	belowOrigin: true,
+	alignment: 'left'
 });
+
+$('.modal-trigger').leanModal();
